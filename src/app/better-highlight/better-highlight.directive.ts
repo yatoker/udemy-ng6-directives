8afd50fe3b4736c8e79a4b15aa0777810deb9584
yatoker <yusufandre@gmail.com>
2018-08-22 11:49:19 +0300

    Binding to Directive Properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -4,7 +4,8 @@ import {
   OnInit,
   ElementRef,
   HostListener,
-  HostBinding
+  HostBinding,
+  Input
 } from '@angular/core';
 
 @Directive({
@@ -12,25 +13,32 @@ import {
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
-
-  ngOnInit(): void {
-    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "blue");
-  }
-
   // Using @HostBinding is even the better way of manipulating the dom element for which directive is used.
   @HostBinding('style.color') foreColor: string = 'black';
 
+  @Input('appBetterHighlight') defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { 
+
+  }
+
+  ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "blue");
+
+    this.renderer.setStyle(this.elRef.nativeElement, "background-color", this.defaultColor);
+  }
+
   //When an element using this directive emits mouseenter event, @HostListener("mouseenter") will be informed about the event and run the code.
   @HostListener("mouseenter") mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color", "blue");
+    this.renderer.setStyle(this.elRef.nativeElement, "background-color", this.highlightColor);
 
     this.foreColor = "white";
   }
 
   @HostListener("mouseleave") mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color", "transparent");
-    
+    this.renderer.setStyle(this.elRef.nativeElement, "background-color", this.defaultColor);
+
     this.foreColor = "black";
   }
 }
